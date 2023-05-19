0ad02f3838638d74c0f0697f24458ca05293666f
Dave Tucker <dave@dtucker.co.uk>
2023-05-19 13:53:10 +0100

    Simplify the probe program
    - Optional prometheus export
    - Optional usdt export
    - Enable gin release mode
    - Use our own metrics registry so we only expose our own metrics in
      prometheus
    
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+//		#include <stdint.h>
+//		#include <sys/sdt.h>
+//
+//      const char* provider = "harness";
+//      const char* requests_total = "requests_total";
+//
+//		static void set_requests_total(uint64_t arg) {
+//			DTRACE_PROBE1(provider, requests_total, arg);
+//		}
+import "C"
+
 import (
 	"flag"
 	"fmt"
@@ -10,10 +21,54 @@ import (
 	"syscall"
 
 	"github.com/gin-gonic/gin"
-	"github.com/penglongli/gin-metrics/ginmetrics"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+var useProm = flag.Bool("prometheus", false, "use prometheus")
+var useUsdt = flag.Bool("usdt", false, "use usdt")
+
+type metrics struct {
+	requestsTotal prometheus.Counter
+}
+
+func NewMetrics(reg prometheus.Registerer) *metrics {
+	m := &metrics{
+		requestsTotal: prometheus.NewCounter(prometheus.CounterOpts{
+			Name: "requests_total",
+			Help: "Number of requests processed by the server, regardless of success or failure",
+		}),
+	}
+	reg.MustRegister(m.requestsTotal)
+	return m
+}
+
+var (
+	metricRequestsTotal uint64 = 0
+)
+
+func udstInterceptor(ctx *gin.Context) {
+	//	startTime := time.Now()
+
+	// execute normal process.
+	ctx.Next()
+
+	// after request
+	metricRequestsTotal++
+	C.set_requests_total(C.ulong(metricRequestsTotal))
+}
+
+func prometheusInterceptor(m *metrics) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		if ctx.Request.URL.Path == "/metrics" {
+			ctx.Next()
+			return
+		}
+		ctx.Next()
+		m.requestsTotal.Inc()
+	}
+}
 
 func main() {
 	var f *os.File
@@ -27,25 +82,31 @@ func main() {
 		pprof.StartCPUProfile(f)
 	}
 
-	appRouter := gin.Default()
-	metricRouter := gin.Default()
+	gin.SetMode(gin.ReleaseMode)
 
-	m := ginmetrics.GetMonitor()
-	// use metric middleware without expose metric path
-	m.UseWithoutExposingEndpoint(appRouter)
-	// set metric path expose to metric router
-	m.Expose(metricRouter)
+	r := gin.Default()
 
-	appRouter.GET("/widgets/:id", func(ctx *gin.Context) {
+	if *useProm {
+		reg := prometheus.NewRegistry()
+		m := NewMetrics(reg)
+		r.Use(prometheusInterceptor(m))
+		r.GET("/metrics", func(ctx *gin.Context) {
+			promhttp.HandlerFor(reg, promhttp.HandlerOpts{Registry: reg}).ServeHTTP(ctx.Writer, ctx.Request)
+		})
+	}
+
+	if *useUsdt {
+		r.Use(udstInterceptor)
+	}
+
+	r.GET("/widgets/:id", func(ctx *gin.Context) {
 		ctx.JSON(200, map[string]string{
 			"widgetId": ctx.Param("id"),
 		})
 	})
+
 	go func() {
-		_ = metricRouter.Run(":9090")
-	}()
-	go func() {
-		_ = appRouter.Run(":8080")
+		_ = r.Run(":8080")
 	}()
 
 	c := make(chan os.Signal, 2)
